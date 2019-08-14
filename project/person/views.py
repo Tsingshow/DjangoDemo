fa5d6d410da2abc88e635f61a2adae99be2e4d41
zmq <z@m.q>
2019-08-14 22:50:28 +0800

    show

--- project/person/views.py
+++ project/person/views.py
@@ -6,6 +6,7 @@ from django.http import request
 from django.http import HttpResponse
 from django.shortcuts import render
 from person.models import Person
+import json
 # Create your views here.
 
 def index(request):
@@ -16,7 +17,23 @@ def data(request):
     age = form_data.get('age')
     birth = form_data.get('birth')
     Person.objects.create(name=name, age=age, birth=birth)
-    print(name)
-    print(age)
-    print(birth)
-    return HttpResponse('ok')
+    response_data = {
+        'name': name,
+        'age': age,
+        'birth': birth
+    }
+    return HttpResponse(json.dumps(response_data))
+
+def emit(request):
+    form_data = request.POST
+    print(form_data)
+    name = form_data.get('name')
+    age = form_data.get('age')
+    birth = form_data.get('birth')
+    Person.objects.create(name=name, age=age, birth=birth)
+    response_data = {
+        'name': name,
+        'age': age,
+        'birth': birth
+    }
+    return render(request, 'show.html', locals())
